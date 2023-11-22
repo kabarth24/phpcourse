de8d9e790a432a4790c8d4a63104a7bac7df36d4
Luka Barth <138171128+kabarth24@users.noreply.github.com>
2023-11-22 04:59:01 -0300

    Add files via upload
    I learned how to declare variables and constants, as well as knowing good practices in PHP

---
+++ ex001/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dados do servidor</title>
+</head>
+
+<body>
+  <h1>Dados do servidor</h1>
+
+  <?php
+  $nome = "Luka";
+  $sobrenome = "Barth";
+
+  const PAIS = "Brasil";
+
+  echo "Muito prazer, $nome $sobrenome! Você mora no " . PAIS;
+  ?>
+</body>
+
+</html>
